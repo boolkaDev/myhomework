bf0222c8b3ff6ee6cc7cb5538c91924683930cb3
boolkaDev <132288482+boolkaDev@users.noreply.github.com>
2023-05-31 22:07:37 +0600

    homework number 1
    el homework de numero uno

---
+++ hw1.py
@@ -0,0 +1,10 @@
+number = int(input("Type 3 digit number: "))
+number3 = number % 10
+number //= 10
+number2 = number % 10
+number //= 10
+number1 = number
+
+print(number1)
+print(number2)
+print(number3)
